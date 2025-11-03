b97cfc8005d3234a73c44bb842ee19256f1cbf18
elfachero3 <elfachero3@gmail.com>
2025-11-03 14:58:54 -0700

    Create init.py

---
+++ ui/init.py
@@ -0,0 +1,22 @@
+# UI module initialization
+from .components import (
+    inject_custom_css,
+    render_header,
+    render_sidebar_config,
+    render_name_inputs,
+    render_audio_uploads,
+    render_text_inputs,
+    render_downloads,
+    render_footer
+)
+
+__all__ = [
+    'inject_custom_css',
+    'render_header',
+    'render_sidebar_config',
+    'render_name_inputs',
+    'render_audio_uploads',
+    'render_text_inputs',
+    'render_downloads',
+    'render_footer'
+]
